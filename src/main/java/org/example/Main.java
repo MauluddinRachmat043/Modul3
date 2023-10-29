b7d7e9f70c8334e6c8ef7ceeac927fa0cd210a32
MauluddinRachmat043 <mauluddinrachmat@gmail.com>
2023-10-29 11:30:20 +0700

    Add README and Code in Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.apache.commons.lang3.StringUtils;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Git!");
+        String text = "Hello, Maven!";
+        String reversedText = StringUtils.reverse(text);
+        System.out.println(reversedText);
     }
 }
